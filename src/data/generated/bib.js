a8f31464ba7772d762f51ca8600a73cc20983ac6
kunalbafna21 <62295967+programmingknowledege@users.noreply.github.com>
2024-05-17 04:35:49 +0100

    Add files via upload

---
+++ src/data/generated/bib.js
@@ -0,0 +1,112 @@
+﻿define({ entries : {
+    "batuwatta2022physics": {
+        "author": "Batuwatta-Gamage, CP and Rathnayaka, CM and Karunasena, H Charminda P and Wijerathne, WDCC and Jeong, Hyogu and Welsh, ZG and Karim, MA and Gu, YT",
+        "journal": "Journal of Food Engineering",
+        "keywords": "type: Research, food drying, machine learning, Deep neural network, Food drying, Artificial Neural network, Moisture concentration , Physics-informed neural network , Shrinkage , Surrogate framework",
+        "pages": "111137",
+        "publisher": "Elsevier",
+        "title": "A physics-informed neural network-based surrogate framework to predict moisture concentration and shrinkage of a plant cell during drying",
+        "type": "article",
+        "volume": "332",
+        "year": "2022"
+    },
+    "ccakmak2011prediction": {
+        "author": "{\\c{C}}akmak, G{\\\"u}l{\\c{s}}ah and Y{\\i}ld{\\i}z, Cengiz",
+        "journal": "Computers and Electronics in Agriculture",
+        "keywords": "type: Research, other:food drying, Seedy grape, Drying,machine learning, Modelling, Artificial Neural network, Predictive Analysis, Agricultural Drying",
+        "number": "1",
+        "pages": "132--138",
+        "publisher": "Elsevier",
+        "title": "The prediction of seedy grape drying rate using a neural network method",
+        "type": "article",
+        "volume": "75",
+        "year": "2011"
+    },
+    "chen2020artificial": {
+        "author": "Chen, Jieling and Zhang, Min and Xu, Baoguo and Sun, Jincai and Mujumdar, Arun S",
+        "journal": "Trends in Food Science \\& Technology",
+        "keywords": "type: Survey, food drying, machine learning,Artificial neural network, Fuzzy logic , Computer vision, Microwave drying",
+        "pages": "251--260",
+        "publisher": "Elsevier",
+        "title": "Artificial intelligence assisted technologies for controlling the drying of fruits and vegetables using physical fields: A review",
+        "type": "article",
+        "volume": "105",
+        "year": "2020"
+    },
+    "hernandez2004neural": {
+        "author": "Hernandez-Perez, JA and Garc{\\i}a-Alvarado, MA and Trystram, G and Heyd, B",
+        "journal": "Innovative Food Science \\& Emerging Technologies",
+        "keywords": "type: Research, food drying, machine learning,Artificial Neural Network, Drying , Cassava, Mango, Shrinkage, Heat and mass Transfer",
+        "number": "1",
+        "pages": "57--64",
+        "publisher": "Elsevier",
+        "title": "Neural networks for the heat and mass transfer prediction during drying of cassava and mango",
+        "type": "article",
+        "volume": "5",
+        "year": "2004"
+    },
+    "iheonye2023monitoring": {
+        "author": "Iheonye, Anthony C and Raghavan, Vijaya and Ferrie, Frank P and Orsat, Val{\\'e}rie and Gariepy, Yvan",
+        "journal": "Food Engineering Reviews",
+        "keywords": "type: Research, food drying, machine learning, Computer vision , Color , Texture , Shrinkage , Deep learning , Real-time monitoring , Drying",
+        "number": "2",
+        "pages": "242--260",
+        "publisher": "Springer",
+        "title": "Monitoring Visual Properties of Food in Real Time During Food Drying",
+        "type": "article",
+        "volume": "15",
+        "year": "2023"
+    },
+    "khan2022machine": {
+        "author": "Khan, Md Imran H and Sablani, Shyam S and Nayak, Richi and Gu, Yuantong",
+        "journal": "Comprehensive reviews in food science and food safety",
+        "keywords": "type: Survey, Food drying, baking, canning, drying, frying, Machine Learning, physics-informed ML models",
+        "number": "2",
+        "pages": "1409--1438",
+        "publisher": "Wiley Online Library",
+        "title": "Machine learning-based modeling in food processing applications: State of the art",
+        "type": "article",
+        "volume": "21",
+        "year": "2022"
+    },
+    "khan2023novel": {
+        "author": "Khan, M Imran H and Longa, Duval and Sablani, Shyam S and Gu, YuanTong",
+        "journal": "Food and Bioprocess Technology",
+        "keywords": "type: Research, Food drying , Artificial Neural Network,  Elastic modulus , Hardness , Stiffness, Machine Learning",
+        "number": "2",
+        "pages": "420--433",
+        "publisher": "Springer",
+        "title": "A novel machine learning-based approach for characterising the micromechanical properties of food material during drying",
+        "type": "article",
+        "volume": "16",
+        "year": "2023"
+    },
+    "li2019neural": {
+        "author": "Li, Honglu and Chen, Songlin",
+        "journal": "Drying technology",
+        "keywords": "type: Research, food drying, machine learning, Counter-current flow grain dryer, thin-layer drying model,  Artificial Neural Network, nonlinear autoregressive exogenous model, model predictive control, particle swarm optimization",
+        "publisher": "Taylor \\& Francis",
+        "title": "A neural-network-based model predictive control scheme for grain dryers",
+        "type": "article",
+        "year": "2019"
+    },
+    "md2003artificial": {
+        "author": "Md. Raisul Islam, Md Raisul Islam and Sablani, SS and Mujumdar, AS",
+        "keywords": "type: Research, food drying, machine learning, Artificial Neural Network, Diffusion Model; Liquid diffusivity.",
+        "title": "An artificial neural network model for prediction of drying rates.",
+        "type": "article",
+        "year": "2003"
+    },
+    "menlik2010determination": {
+        "author": "Menlik, Tayfun and {\\\"O}zdemir, Mustafa Bahad{\\i}r and Kirmaci, Volkan",
+        "journal": "Expert Systems with Applications",
+        "keywords": "type: Research, food drying, machine learning,Apples Freeze drying, Food Drying, Modeling, Artificial Neural Network",
+        "number": "12",
+        "pages": "7669--7677",
+        "publisher": "Elsevier",
+        "title": "Determination of freeze-drying behaviors of apples by artificial neural network",
+        "type": "article",
+        "volume": "37",
+        "year": "2010"
+    }
+}});
